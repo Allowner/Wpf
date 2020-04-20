fd414b544f5946833ac36f48856d3ab2b8db7de0
Vsevolod <vsevolod.gordienko99@gmail.com>
2020-04-20 19:37:50 +0300

    Не успел сделать следуя верному разделению на слои

---
+++ PhoneBook.Domain/ContactsContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class ContactsContext : DbContext
+    {
+        public ContactsContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
